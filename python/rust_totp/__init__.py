717afca7f7c16c280cacc531f9672340cb24fafe
Vizonex <114684698+Vizonex@users.noreply.github.com>
2026-05-20 22:26:56 -0500

    Add version string even though `Cargo.toml` has the real version number.

--- python/rust_totp/__init__.py
+++ python/rust_totp/__init__.py
@@ -3,6 +3,7 @@ from .rust_totp import (TOTP, Algorithm, QRCodeError, RFC6238Error,
                         new_secret)
 
 __author__ = "Vizonex"
+__version__ = "0.1.0"
 
 __all__ = (
     "Algorithm",
